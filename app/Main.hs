a5859be9350de379565cbeccfe355163a44817cf
nstall-unm <nstall115@unm.edu>
2025-04-28 22:41:13 -0600

    Brillo blank window

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,21 @@ module Main where
 
 import Brillo
 
--- testing
+window :: Display
+window = InWindow "Window" (1024, 768) (10, 10)
+bg :: Color
+bg = white
+fps :: Int
+fps = 1
+
+data State = State deriving (Eq, Show)
+
+initState :: State
+initState = State
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
-
-{-
-Scott was here
-Ayman was here
--}
+main = 
+    simulate window bg fps 
+        initState 
+        (\_ -> blank)
+        (\_ _ s -> s)
