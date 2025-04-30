1b571db44b687dc1c579dd2d632a2c8d07d1fe3e
nstall-unm <nstall115@unm.edu>
2025-04-30 12:25:55 -0600

    Proof of concept animations

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,39 @@
 module Main where
 
 import Brillo
+import Brillo.Data.ViewPort (ViewPort)
 
 window :: Display
 window = InWindow "Window" (1024, 768) (10, 10)
+
 bg :: Color
 bg = white
-fps :: Int
-fps = 1
 
-data State = State deriving (Eq, Show)
+fps :: Int
+fps = 60
+
+data State = State Float deriving (Eq, Show)
 
 initState :: State
-initState = State
+initState = State 400
+
 
 draw :: State -> Picture
-draw _ = Color black (rectangleSolid 100 150)
+draw (State x) = Pictures
+    [ Color black (rectangleSolid 100 150)        -- tower at (0, 0)
+    , Translate x 0 (Color red (circleSolid 20))  -- Circle moves toward 0
+    ]
+
+-- Moves the circle
+update :: ViewPort -> Float -> State -> State
+update _ _ (State x)
+    | x > 0     = State (x - 1)
+    | otherwise = State x
 
 main :: IO ()
 main =
     simulate window bg fps
         initState
         draw
-        (\_ _ s -> s)
+        update
+        
