cf1c060aa5bf78ceba94458744042cdf0b22e6cc
Scott Morgan <spm3412@gmail.com>
2025-04-30 13:45:38 -0600

    Adding random

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
 import Brillo
+import System.Random (randomRIO)
 
 window :: Display
 window = InWindow "Window" (1024, 768) (10, 10)
+
 bg :: Color
 bg = white
+
 fps :: Int
 fps = 1
 
@@ -14,8 +17,16 @@ data State = State deriving (Eq, Show)
 initState :: State
 initState = State
 
+-- Random Y spawnpoint (on left edge)
+-- initState :: IO State
+-- initState = do
+--     y <- 
+
 draw :: State -> Picture
-draw _ = Color black (rectangleSolid 100 150)
+--draw _ = Color green (rectangleSolid 300 250)
+draw _ = Pictures   
+        [ Translate 0 0 $ Color green (rectangleSolid 300 250)
+        , translate (-512) 0 $ Color blue (circleSolid 20)]
 
 main :: IO ()
 main =
