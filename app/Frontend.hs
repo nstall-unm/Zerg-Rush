c07fa62a8167e4e64f5b83c0b123f3dc51b53c3c
nstall-unm <nstall115@unm.edu>
2025-05-01 10:34:35 -0600

    Figured out how to draw things in Frontend.hs

--- app/Frontend.hs
+++ app/Frontend.hs
@@ -1,11 +1,24 @@
 module Frontend where
 
+import Brillo
 import Brillo.Data.Picture
 
 import Types
 
+-- draw :: State -> Picture
+-- draw s = Blank
+
 draw :: State -> Picture
-draw s = Blank
+draw s = Pictures
+     [ Color black (rectangleSolid 100 150) 
+     , (Color red (circleSolid 20))]
+
+-- draw :: State -> Picture
+-- draw _ = Color black (rectangleSolid 100 150)
+-- draw (State x) = Pictures
+--      [ Color black (rectangleSolid 100 150)        -- tower at (0, 0)
+--      , Translate x 0 (Color red (circleSolid 20))  -- Circle moves toward 0
+--      ]
 
 --
 
